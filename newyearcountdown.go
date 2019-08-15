a58cb66ecece4e8d562f6671ce0544747a64b75f
David Šorm <david.sorm@gmail.com>
2019-08-15 12:22:00 +0200

    improvement by switching to ticker

--- newyearcountdown.go
+++ newyearcountdown.go
@@ -7,9 +7,8 @@ import (
 
 func main() {
 	newYearUnix := time.Date(time.Now().Year()+1, 1, 1, 0, 0, 0, 0, time.Local).Unix()
-	for {
-		time.Sleep(1 * time.Second)
-		currentUnix := time.Now().Unix()
-		fmt.Printf("\r%v s", newYearUnix-currentUnix)
+	ticker := time.NewTicker(1 * time.Second)
+	for range ticker.C {
+		fmt.Printf("\r%v s", newYearUnix-time.Now().Unix())
 	}
 }
