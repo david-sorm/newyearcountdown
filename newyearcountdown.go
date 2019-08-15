ba9bb10def914bba9e8772632b405d610dce4e4b
David Šorm <david.sorm@gmail.com>
2019-08-15 12:16:15 +0200

    improvements

--- newyearcountdown.go
+++ newyearcountdown.go
@@ -5,15 +5,11 @@ import (
 	"time"
 )
 
-func refresh(newYearUnix int64) {
-	currentUnix := time.Now().Unix()
-	go fmt.Printf("\r%v s", newYearUnix-currentUnix)
-}
-
 func main() {
 	newYearUnix := time.Date(time.Now().Year()+1, 1, 1, 0, 0, 0, 0, time.Local).Unix()
 	for {
 		time.Sleep(1 * time.Second)
-		go refresh(newYearUnix)
+		currentUnix := time.Now().Unix()
+		fmt.Printf("\r%v s", newYearUnix-currentUnix)
 	}
 }
